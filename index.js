5ef0f68d353eb5e719c203518ddfa3c75275d62b
swastik_khatua <swastikkhatua4@gmail.com>
2024-09-02 15:43:49 +0530

    hello

---
+++ index.js
@@ -0,0 +1,13 @@
+let a = prompt("entervalue");
+if (a<500) {
+    console.log("Less thene 500");
+}
+else if (a>500){
+    console.log("Greater then 500");
+}
+else if (a=500){
+    console.log("Equal to 500");
+}
+else {
+    console.log("Invalid");
+}
